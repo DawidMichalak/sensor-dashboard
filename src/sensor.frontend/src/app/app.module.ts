eb0c77709533aa7da2763a8619feb9153a8cda82
Dawid Michalak <dawid.michalak0905@gmail.com>
2023-04-02 12:33:59 +0200

    Add sample chart

--- src/sensor.frontend/src/app/app.module.ts
+++ src/sensor.frontend/src/app/app.module.ts
@@ -12,12 +12,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { SensorReadingsComponent } from './sensor-readings/sensor-readings.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SensorReadingsComponent
+    SensorReadingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +30,7 @@ import { SensorReadingsComponent } from './sensor-readings/sensor-readings.compo
     MatButtonModule,
     LayoutModule,
     HttpClientModule,
+    NgChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/sensor.frontend/src/app/sensor-readings/sensor-readings.component.ts
+++ src/sensor.frontend/src/app/sensor-readings/sensor-readings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SensorDataService } from '../sensor-data.service';
 import { Reading } from './reading';
+import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
 
 @Component({
   selector: 'app-sensor-readings',
@@ -16,4 +17,31 @@ export class SensorReadingsComponent implements OnInit{
   ngOnInit(): void {
     this.dataService.getReadings().subscribe(readings => this.readings = readings);
   }
+
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
 }
