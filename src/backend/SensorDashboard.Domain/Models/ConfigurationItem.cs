5fa453c3afae459b76ef3da05486014b988b282d
DawidMichalak <dawid.michalak0905@gmail.com>
2023-04-27 10:17:34 +0200

    Add models for dashboard configuration

---
+++ src/backend/SensorDashboard.Domain/Models/ConfigurationItem.cs
@@ -0,0 +1,20 @@
+﻿namespace SensorDashboard.Domain.Models
+{
+    public sealed class ConfigurationItem
+    {
+        public string? Id { get; set; }
+        public string Name { get; }
+        public int SensorId { get; }
+
+        public ConfigurationItem(string name, int sensorId)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name cannot be null or empty", nameof(name));
+            }
+
+            Name = name;
+            SensorId = sensorId;
+        }
+    }
+}
