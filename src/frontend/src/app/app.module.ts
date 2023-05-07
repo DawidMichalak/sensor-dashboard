4da748dee6c6a7b3465e12cdd1f0ddccb254d3b0
DawidMichalak <dawid.michalak0905@gmail.com>
2023-05-07 17:05:09 +0200

    Add shared module

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
-import {
-  MAT_FORM_FIELD_DEFAULT_OPTIONS,
-  MatFormFieldModule,
-} from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SensorReadingsComponent } from './sensor-readings/sensor-readings.component';
@@ -21,12 +9,10 @@ import { DateRangeSelectionComponent } from './sensor-readings/date-range-select
 import { ReadingsMenuComponent } from './sensor-readings/readings-menu/readings-menu.component';
 import { RenameDialogComponent } from './sensor-readings/readings-menu/rename-dialog/rename-dialog.component';
 import { DeleteDialogComponent } from './sensor-readings/readings-menu/delete-dialog/delete-dialog.component';
-import { MatInputModule } from '@angular/material/input';
 import { NavbarComponent } from './navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSelectModule } from '@angular/material/select';
 import { AddCardDialogComponent } from './navbar/add-card-dialog/add-card-dialog.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -43,27 +29,11 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     ReactiveFormsModule,
     FormsModule,
-    MatChipsModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    MatSlideToggleModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatDialogModule,
-    MatInputModule,
-    MatToolbarModule,
-    MatSelectModule,
     HttpClientModule,
+    SharedModule,
   ],
-  providers: [
-    {
-      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: { appearance: 'outline' },
-    },
-  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
